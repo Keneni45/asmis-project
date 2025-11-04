000c96e29cb24441719e201f35903356dba193dd
Keneni45 <kenotoneaga@gmail.com>
2025-11-04 06:58:21 +0100

    feat: add static route file

--- asmis/urls.py
+++ asmis/urls.py
@@ -1,10 +1,5 @@
-
-#from django.contrib import admin
-#from django.urls import path
-
-#urlpatterns = [
-#    path('admin/', admin.site.urls),
-#]
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -12,5 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('asmisapp.urls')),
 ]
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
